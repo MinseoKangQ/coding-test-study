0e0fb116bae0c96175769462322a29c5fbf3a884
Minseo Kang <98332877+MinseoKangQ@users.noreply.github.com>
2023-01-21 20:28:05 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -4,16 +4,9 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        while(true) {
-            if(n >= 10) {
-                answer = answer + (n%10);
-                n /= 10;
-            }
-            else {
-                answer = answer + (n%10);
-                n /= 10;
-                break;
-            }
+        while(n != 0) {
+            answer = answer + (n%10);
+            n /= 10;
         }
 
         return answer;
