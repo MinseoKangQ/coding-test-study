a096fcd622f2726226c73c57986772ed1a26d454
Minseo Kang <98332877+MinseoKangQ@users.noreply.github.com>
2023-01-31 22:41:11 +0900

    [Bronze V] Title: A+B - 4, Time: 228 ms, Memory: 17968 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.hasNextInt()) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            System.out.println(x+y);
+        }
+        
+        scanner.close();
+        
+    }
+
+}
