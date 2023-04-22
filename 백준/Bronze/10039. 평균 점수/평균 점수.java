047c8f3b6885e08c284eb83cd77e1696b79d2e9d
Minseo Kang <98332877+MinseoKangQ@users.noreply.github.com>
2023-04-23 01:54:58 +0900

    [Bronze IV] Title: 평균 점수, Time: 212 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+
+        for(int i = 0; i < 5; i++) {
+            int score = scanner.nextInt();
+            if (score < 40) sum += 40;
+            else sum += score;
+        }
+
+        System.out.println(sum/5);
+    }
+}
