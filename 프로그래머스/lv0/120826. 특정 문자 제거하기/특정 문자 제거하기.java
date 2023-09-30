67a99d5aaa06dc489caac00e1dce9c6516863db9
Minseo Kang <98332877+MinseoKangQ@users.noreply.github.com>
2023-10-01 01:26:40 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.09 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = "";
+        
+        for(int i = 0; i < my_string.length(); i++) {
+            char currentChar = my_string.charAt(i);
+            if (currentChar != letter.charAt(0)) {
+                answer = answer.concat(Character.toString(currentChar));
+            }
+        }
+        return answer;
+    }
+}
