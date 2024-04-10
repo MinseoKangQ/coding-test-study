8cd973b212f9f9c12d9d36d8df0930931dd515b0
Minseo Kang <kms02171@naver.com>
2024-04-10 17:24:57 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,81 +1,57 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
 
 public class Main {
+
+    public static String solution(String str) {
+
+        String answer = "YES";
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : str.toCharArray()) {
+
+            if (c == '(') stack.push(c);
+            else {
+                if (stack.isEmpty()) {
+                    answer = "NO";
+                    break;
+                } else {
+                    stack.pop();
+                }
+            }
+
+        }
+
+        if (!stack.isEmpty()) {
+            answer = "NO";
+        }
+
+        return answer;
+
+    }
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < N; i++) {
-
-            boolean isTrue = true;
+        for(int i = 0; i < T; i++) {
             String str = br.readLine();
-
-            // 스택 생성 및 초기화
-            Stack stack = new Stack(str.length());
-
-
-            // 괄호 검사
-            for(int j = 0; j < str.length(); j++) {
-
-                char ch, open_ch;
-
-                ch = str.charAt(j);
-
-                switch (ch) {
-                    case '(':
-                        stack.push(ch);
-                        break;
-                    case ')' :
-                        if(stack.isEmpty()) isTrue = false;
-                        else {
-                            open_ch = stack.pop();
-                            if(open_ch == '(' && ch != ')') {
-                                isTrue = false;
-                            }
-                            break;
-                        }
-                }
-
-            }
-
-            if(!stack.isEmpty() || isTrue == false) System.out.println("NO");
-            else System.out.println("YES");
-
+            String answer = solution(str);
+            sb.append(answer).append("\n");
         }
+
+        bw.write(sb.toString());
+        bw.flush();
+
+        bw.close();
+        br.close();
     }
-}
-
-class Stack {
-
-    private int top;
-    private int size;
-    private Character arr [];
-
-    public Stack(int size) {
-        this.top = -1;
-        this.size = size;
-        this.arr = new Character[this.size];
-    }
-
-    public boolean isEmpty() { return (top == -1); }
-    public boolean isFull() { return (top == this.size-1); }
-
-    public void push(Character item) {
-        if(!isFull()) {
-            arr[++top] = item;
-        }
-    }
-
-    public Character pop() {
-        if(!isEmpty()) {
-            return (arr[top--]);
-        }
-        else return 'e';
-    }
-
 }
