5837f7ce793bcb633b8194815f4c59f92535fd2e
Minseo Kang <kms02171@naver.com>
2024-04-15 23:59:41 +0900

    [Silver V] Title: 부분 문자열, Time: 200 ms, Memory: 16272 KB -BaekjoonHub

--- 백준/Silver/6550. 부분 문자열/부분 문자열.java
+++ 백준/Silver/6550. 부분 문자열/부분 문자열.java
@@ -8,30 +8,43 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static String solution(String s, String t) {
-        int indexS = 0;
-        for (int i = 0; i < t.length() && indexS < s.length(); i++) {
-            if (s.charAt(indexS) == t.charAt(i)) {
-                indexS++;
-            }
-        }
+    public static String solution(String str1, String str2) {
 
-        if (indexS == s.length()) {
-            return "Yes";
-        } else {
-            return "No";
+        String answer = "Yes";
+
+        if (str2.contains(str1)) return answer;
+        else {
+
+            Stack<Character> stack = new Stack<>();
+            for(int i = str1.length()-1; i >= 0; i--) {
+                stack.push(str1.charAt(i));
+            }
+
+            for(int i = 0; i < str2.length(); i++) {
+                if (stack.isEmpty()) return "Yes";
+                char c = str2.charAt(i);
+                if (stack.peek() == c) {
+                    stack.pop();
+                }
+            }
+
+            if (stack.isEmpty()) return "Yes";
+            else return "No";
+
         }
+        
     }
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuffer sb = new StringBuffer();
 
         String inputLine;
-        while ((inputLine = br.readLine()) != null && !inputLine.isEmpty()) {
+        while ((inputLine = br.readLine()) != null && !inputLine.isEmpty()) { // 입력의 끝을 null 또는 빈 문자열로 판단
             StringTokenizer st = new StringTokenizer(inputLine, " ");
-            if (st.countTokens() == 2) {
+            if (st.countTokens() == 2) { // 정확히 두 개의 토큰이 있는지 확인
                 String s = st.nextToken();
                 String t = st.nextToken();
                 sb.append(solution(s, t)).append("\n");
@@ -40,7 +53,9 @@ public class Main {
 
         bw.write(sb.toString());
         bw.flush();
+
         bw.close();
         br.close();
+
     }
 }
