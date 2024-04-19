fcbd332bb9b65e0ef916dfa0669870eed994def9
Minseo Kang <kms02171@naver.com>
2024-04-19 23:12:18 +0900

    [Silver V] Title: 짐 챙기는 숌, Time: 124 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/1817. 짐 챙기는 숌/짐 챙기는 숌.java
+++ 백준/Silver/1817. 짐 챙기는 숌/짐 챙기는 숌.java
@@ -21,24 +21,22 @@ public class Main {
         if (N != 0) {
             int minCount = 1; // 1개부터 시작
             int currentBoxWeight = 0;
-
-            int [] arr = new int[N];
+            
             st = new StringTokenizer(br.readLine(), " ");
 
-            for(int i = 0; i < N; i++) {
-                arr[i] = Integer.parseInt(st.nextToken());
-            }
-
             int index = 0;
+            
             while(true) {
 
                 if(index == N) break;
+                
+                int x = Integer.parseInt(st.nextToken());
 
-                if (currentBoxWeight + arr[index] > M) { // 넘으면 못담음, 다음 상자에 담아야 함
-                    currentBoxWeight = arr[index];
+                if (currentBoxWeight + x > M) { // 넘으면 못담음, 다음 상자에 담아야 함
+                    currentBoxWeight = x;
                     minCount++;
                 } else { // 그렇지 않으면 담을 수 있음
-                    currentBoxWeight += arr[index];
+                    currentBoxWeight += x;
                 }
 
                 index++;
