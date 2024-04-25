85772f430b8b3d12b094843d44626da5e40ba890
Minseo Kang <kms02171@naver.com>
2024-04-25 21:50:07 +0900

    [Silver II] Title: 쇠막대기, Time: 184 ms, Memory: 16308 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,39 +1,49 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Stack;
 
 public class Main {
+
+    public static int solution(String str) {
+
+        int count = 0;
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') { // 여는 괄호 만나면
+                stack.push('('); // push
+            }
+            else { // 닫는 괄호 만나면
+                stack.pop(); // 일단 pop
+                if (str.charAt(i-1) == '(') { // 바로 앞이 여는 괄호면 레이저 쏨
+                    count += stack.size();
+                }
+                else { // 바로 앞이 닫는 괄호면 막대기의 끝
+                    count++;
+                }
+            }
+        }
+
+        return count;
+
+    }
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String input = br.readLine();
-        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
 
+        String str = br.readLine();
 
-        int result = 0;
-        for(int i = 0; i < input.length(); i++) {
-            if(input.charAt(i) == '(') {
-                stack.push('(');
-                continue;
-            }
-            if(input.charAt(i) == ')') {
-                stack.pop();
-
-                if(input.charAt(i-1) == '(') {
-                    result += stack.size();
-                }
-                else {
-                    result++;
-                }
-            }
-        }
-
-        bw.write(result + "\n");
+        bw.write(sb.append(solution(str)).toString());
         bw.flush();
-        br.close();
-        bw.close();
 
+        bw.close();
+        br.close();
     }
 }
-
